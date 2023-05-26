808b7edccbb2f1ec02a7d8b8fa44dd09bd58aed0
sawyerbutton <linghui92liu@gmail.com>
2023-05-26 21:53:07 +0800

    update trainer folder for annotated bert pytorch

--- code/annotated_bert_pytorch/trainer/optim_schedule.py
+++ code/annotated_bert_pytorch/trainer/optim_schedule.py
@@ -4,32 +4,34 @@ import numpy as np
 
 class ScheduledOptim():
     '''A simple wrapper class for learning rate scheduling'''
+    # 一个简单的学习率调整的包装类
 
     def __init__(self, optimizer, d_model, n_warmup_steps):
-        self._optimizer = optimizer
-        self.n_warmup_steps = n_warmup_steps
-        self.n_current_steps = 0
-        self.init_lr = np.power(d_model, -0.5)
+        self._optimizer = optimizer # 优化器
+        self.n_warmup_steps = n_warmup_steps # warmup步数
+        self.n_current_steps = 0 # 当前步数
+        self.init_lr = np.power(d_model, -0.5) # 学习率初始化
 
-    def step_and_update_lr(self):
+    def step_and_update_lr(self): # 更新学习率
         "Step with the inner optimizer"
         self._update_learning_rate()
-        self._optimizer.step()
+        self._optimizer.step() # 更新参数
 
-    def zero_grad(self):
+    def zero_grad(self): # 梯度清零
         "Zero out the gradients by the inner optimizer"
         self._optimizer.zero_grad()
 
-    def _get_lr_scale(self):
+    def _get_lr_scale(self): # 获取学习率的缩放
         return np.min([
             np.power(self.n_current_steps, -0.5),
             np.power(self.n_warmup_steps, -1.5) * self.n_current_steps])
+        # 返回 warmup步数的-1.5次方乘以当前步数 和 当前步数的-0.5次方 的最小值
 
-    def _update_learning_rate(self):
+    def _update_learning_rate(self): # 更新学习率
         ''' Learning rate scheduling per step '''
 
-        self.n_current_steps += 1
-        lr = self.init_lr * self._get_lr_scale()
+        self.n_current_steps += 1 # 当前步数加1
+        lr = self.init_lr * self._get_lr_scale() # 学习率 = 学习率初始化 * 学习率缩放
 
-        for param_group in self._optimizer.param_groups:
-            param_group['lr'] = lr
+        for param_group in self._optimizer.param_groups: # 遍历优化器的参数组
+            param_group['lr'] = lr # 更新学习率
--- code/annotated_bert_pytorch/trainer/pretrain.py
+++ code/annotated_bert_pytorch/trainer/pretrain.py
@@ -10,6 +10,7 @@ import tqdm
 
 
 class BERTTrainer:
+
     """
     BERTTrainer make the pretrained BERT model with two LM training method.
 
@@ -17,7 +18,7 @@ class BERTTrainer:
         2. Next Sentence prediction : 3.3.2 Task #2: Next Sentence Prediction
 
     please check the details on README.md with simple example.
-
+    Bert 的预训练任务分为 MLM任务和NSP任务
     """
 
     def __init__(self, bert: BERT, vocab_size: int,
@@ -38,90 +39,95 @@ class BERTTrainer:
 
         # Setup cuda device for BERT training, argument -c, --cuda should be true
         cuda_condition = torch.cuda.is_available() and with_cuda
-        self.device = torch.device("cuda:0" if cuda_condition else "cpu")
+        self.device = torch.device("cuda:0" if cuda_condition else "cpu") # 优先使用GPU
 
         # This BERT model will be saved every epoch
         self.bert = bert
         # Initialize the BERT Language Model, with BERT model
-        self.model = BERTLM(bert, vocab_size).to(self.device)
+        self.model = BERTLM(bert, vocab_size).to(self.device) # 用BERTLM来做预训练任务
 
         # Distributed GPU training if CUDA can detect more than 1 GPU
-        if with_cuda and torch.cuda.device_count() > 1:
+        if with_cuda and torch.cuda.device_count() > 1: # 如果有多个GPU，那么使用并行计算
             print("Using %d GPUS for BERT" % torch.cuda.device_count())
             self.model = nn.DataParallel(self.model, device_ids=cuda_devices)
+            # nn.DataParallel()是并行计算，将模型放到多个GPU上
 
         # Setting the train and test data loader
+        # train_dataloader 是一个DataLoader对象，里面包含了训练数据
         self.train_data = train_dataloader
+        # test_dataloader 是一个DataLoader对象，里面包含了测试数据
         self.test_data = test_dataloader
 
         # Setting the Adam optimizer with hyper-param
-        self.optim = Adam(self.model.parameters(), lr=lr, betas=betas, weight_decay=weight_decay)
+        self.optim = Adam(self.model.parameters(), lr=lr, betas=betas, weight_decay=weight_decay) # 初始化优化器
+        # betas是Adam优化器的参数，weight_decay是L2正则化的参数
         self.optim_schedule = ScheduledOptim(self.optim, self.bert.hidden, n_warmup_steps=warmup_steps)
+        # ScheduledOptim是一个类，用来调整学习率
 
         # Using Negative Log Likelihood Loss function for predicting the masked_token
-        self.criterion = nn.NLLLoss(ignore_index=0)
+        self.criterion = nn.NLLLoss(ignore_index=0) # NLLLoss是负对数似然损失函数，ignore_index=0表示忽略0的损失
 
-        self.log_freq = log_freq
+        self.log_freq = log_freq # log_freq是日志频率
 
-        print("Total Parameters:", sum([p.nelement() for p in self.model.parameters()]))
+        print("Total Parameters:", sum([p.nelement() for p in self.model.parameters()])) # 打印模型参数的数量
 
     def train(self, epoch):
-        self.iteration(epoch, self.train_data)
+        self.iteration(epoch, self.train_data) # 对训练数据进行迭代
 
     def test(self, epoch):
-        self.iteration(epoch, self.test_data, train=False)
+        self.iteration(epoch, self.test_data, train=False) # 对测试数据进行迭代
 
     def iteration(self, epoch, data_loader, train=True):
         """
-        loop over the data_loader for training or testing
-        if on train status, backward operation is activated
-        and also auto save the model every peoch
+        循环遍历数据加载器以进行训练或测试
+        如果处于训练状态，则激活反向操作，并在每个 epoch 自动保存模型
 
-        :param epoch: current epoch index
-        :param data_loader: torch.utils.data.DataLoader for iteration
-        :param train: boolean value of is train or test
-        :return: None
+        参数 epoch：当前 epoch 索引
+        data_loader：用于迭代的 torch.utils.data.DataLoader
+        train：是训练流程还是测试流程的布尔值
+        返回值：无
         """
         str_code = "train" if train else "test"
 
-        # Setting the tqdm progress bar
+        # 设定tqdm进度条
         data_iter = tqdm.tqdm(enumerate(data_loader),
                               desc="EP_%s:%d" % (str_code, epoch),
                               total=len(data_loader),
                               bar_format="{l_bar}{r_bar}")
 
-        avg_loss = 0.0
-        total_correct = 0
-        total_element = 0
+        avg_loss = 0.0 # 平均损失
+        total_correct = 0 # 总正确数
+        total_element = 0 # 总元素数
 
         for i, data in data_iter:
-            # 0. batch_data will be sent into the device(GPU or cpu)
+            # 将 batch_data 发送到设备(GPU 或 CPU)中
             data = {key: value.to(self.device) for key, value in data.items()}
 
-            # 1. forward the next_sentence_prediction and masked_lm model
+            # 前向传播 next_sentence_prediction 和 masked_lm 模型
             next_sent_output, mask_lm_output = self.model.forward(data["bert_input"], data["segment_label"])
 
-            # 2-1. NLL(negative log likelihood) loss of is_next classification result
+            # . is_next 分类结果的 NLL(negative log likelihood) 损失
             next_loss = self.criterion(next_sent_output, data["is_next"])
 
-            # 2-2. NLLLoss of predicting masked token word
+            # 预测掩码标记词的 NLL Loss
             mask_loss = self.criterion(mask_lm_output.transpose(1, 2), data["bert_label"])
 
-            # 2-3. Adding next_loss and mask_loss : 3.4 Pre-training Procedure
+            # 添加 next_loss 和 mask_loss
             loss = next_loss + mask_loss
 
-            # 3. backward and optimization only in train
+            # 仅在训练时进行反向传播和优化
             if train:
                 self.optim_schedule.zero_grad()
                 loss.backward()
                 self.optim_schedule.step_and_update_lr()
 
-            # next sentence prediction accuracy
+            # 下一个句子预测准确率
             correct = next_sent_output.argmax(dim=-1).eq(data["is_next"]).sum().item()
             avg_loss += loss.item()
             total_correct += correct
             total_element += data["is_next"].nelement()
 
+            # 打印日志信息
             post_fix = {
                 "epoch": epoch,
                 "iter": i,
@@ -130,19 +136,19 @@ class BERTTrainer:
                 "loss": loss.item()
             }
 
+            # 每隔 log_freq 打印一次日志信息
             if i % self.log_freq == 0:
                 data_iter.write(str(post_fix))
 
         print("EP%d_%s, avg_loss=" % (epoch, str_code), avg_loss / len(data_iter), "total_acc=",
-              total_correct * 100.0 / total_element)
-
+              total_correct * 100.0 / total_element) # 打印平均损失和总准确率
+        
     def save(self, epoch, file_path="output/bert_trained.model"):
         """
-        Saving the current BERT model on file_path
-
-        :param epoch: current epoch number
-        :param file_path: model output path which gonna be file_path+"ep%d" % epoch
-        :return: final_output_path
+        从文件中保存当前 BERT 模型到指定路径
+        参数 epoch：当前 epoch 索引
+        file_path：模型输出路径，将保存在 file_path+"ep%d" % epoch
+        返回值：最终输出路径
         """
         output_path = file_path + ".ep%d" % epoch
         torch.save(self.bert.cpu(), output_path)
